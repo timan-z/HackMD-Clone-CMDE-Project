82204d35eafdae6d18bb2ce9139fd0966d68177e
timan-z <58091424+timan-z@users.noreply.github.com>
2025-05-06 15:53:22 -0400

    Setup PostgreSQL this morning. Having some issues with my Express server that connects to it but otherwise things are looking good so far.

--- src/components/backend/expressServer.js
+++ src/components/backend/expressServer.js
@@ -1,7 +1,18 @@
+/* This is my backend file for setting up an Express server. Here's where I'll have code for:
+0. Connect to PostgreSQL.
+1. Signing up / Registering users.
+2. Logging in w/Email + Password (DEBUG: When testing, just use one email and have the "+1", "+2" etc cheat for testing multiple accounts).
+3. Authenticating users with JWT Tokens.
+4. Storing session Tokens in the frontend (browser localStorage).
+5. Storing user credentials securely in PostgreSQL w/ Hashed Passwords.
+6. Enable protected frontend routes (login-page, dashboard, editor).
+*/
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from './routes/authentication.js';
+import pg from "pg";
+//import authRoutes from './routes/auth.js';
 
 dotenv.config();
 
@@ -11,8 +22,32 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // Parses incoming JSON requests. 
 
+// Connect to PostgreSQL:
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+// DEBUG: Test the DB connection.
+pool.connect((err, client, release) => {
+    if(err) {
+        console.error("Error acquiring client", err);
+        return;
+    }
+    client.query("SELECT NOW()", (err, result) => {
+        release();
+        if(err) {
+            return console.error("Error executing query", err.stack);
+        }
+        console.log("PostgreSQL connected: ", result.rows);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("Backend is running.");
+});
+
 // Routes:
-app.use("/api/auth", authRoutes);
+//app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`EXPRESS Server is running on port ${PORT}`);
