ad11ccff31862008a734c4dbf6f4853a8ce55d49
timan-z <58091424+timan-z@users.noreply.github.com>
2025-05-12 21:33:46 -0400

    Reorganizing my CSS approach to styling HTML being returned from a .jsx file. Going to define most of the styling in-line now since React will immediately render that styling (doesn't immediately happen sometimes with the .css file because of caching or whatever, which is annoying when actively building and trying to debug stuff)

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -9,39 +9,49 @@ import {register} from "../utility/api.js";
 
 function Register() {
     return(
+        /* The contents of the outermost <div> will be two primary inner <div> elements equally partitioned
+        to the left and right (just like the GitHub Registration webpage as of 5/7/25). In the <div> below, there will
+        be some sort of design and interactive Text advertising the site -- while, the actual Registration form will be to its right. */
 
-
-
-
-
-        <div id="registerp-outermost-div">
-
-
-
-
-            {/* The contents of the outermost <div> will be two primary inner <div> elements equally partitioned
-            to the left and right (just like the GitHub Registration webpage as of 5/7/25). In the <div> below, there will
-            be some sort of design and interactive Text advertising the site -- while, the actual Registration form will be to its right. */}
-            
+        <div id="registerp-outermost-div" style={{display:"flex",height:"98vh",width:"99vw",position:"absolute", overflow:"hidden", borderStyle:"bold", borderColor:"red"}}> {/* <-- DEBUG: Don't forget to remove borderStyle:"bold" onwards later. */}
             {/* 1. LHS: */}
-            <div id="registerp-lhs"> {/* <--DEBUG: solid borderStyle is */}
+            <div id="registerp-lhs" style={{height:"100%",width:"50%", borderStyle:"bold", borderColor:"orange"}}> {/* <-- DEBUG: Don't forget to remove borderStyle:"bold" onwards later. */}
                 SOMETHING GOES HERE!!! (PROBABLY JUST HAVE SOME FANCY VISUAL DESIGN HERE).
             </div>
 
             {/* 2. RHS: */}
-            <div id="registerp-rhs" style={{borderStyle:"solid"}} >
-
+            <div id="registerp-rhs" style={{height:"100%", width:"50%", borderStyle:"bold", borderColor:"blue"}}> {/* <-- DEBUG: Don't forget to remove borderStyle:"bold" onwards later. */}
                 {/* At the top row of the page's RHS will be a comment re-directing to the Login page. */}
                 <div style={{textAlign:"right", borderStyle:"solid"}}>
                     Already have an account? <a href="Login">Sign in →</a>
                 </div>
-                
-                {/* Beneath that is  */}
+
+
+
                 <div id="register-outermost-box">
-                    some more code.
+                
+                </div>
+            </div>
+
+
+
+            {/* 1. LHS:
+            <div id="registerp-lhs" style={{borderStyle:"solid"}}> * <--DEBUG: solid borderStyle is *
+                SOMETHING GOES HERE!!! (PROBABLY JUST HAVE SOME FANCY VISUAL DESIGN HERE).
+            </div>*/}
+            {/* 2. RHS: *
+            <div id="registerp-rhs" style={{borderStyle:"solid"}} >
+                * At the top row of the page's RHS will be a comment re-directing to the Login page. *
+                <div style={{textAlign:"right", borderStyle:"solid"}}>
+                    Already have an account? <a href="Login">Sign in →</a>
                 </div>
                 
-            </div>
+                * Beneath that is  *
+                <div id="register-outermost-box">
+                
+                </div>
+            </div> */}
+
 
 
         </div>
