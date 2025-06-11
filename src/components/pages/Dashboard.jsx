dae4fb9ff8194c817a3c8c5091eec39fe788dce6
timan-z <58091424+timan-z@users.noreply.github.com>
2025-06-10 23:14:47 -0400

    Cleaned up the Dashboard.jsx file.

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,12 +1,6 @@
-// Dashboard for the Editing Sessions goes here (+ other customization like "Join Room") -- home page if logged in.
-//<h1>DASHBOARD GOES HERE!!!</h1>
-
-import React, {useEffect, useState, useRef} from "react";
-import { Navigate, useNavigate } from "react-router-dom";
-import { createNewEdRoom, getAllRooms, checkRoomAccess, generateInvLink, joinRoomViaInv, leaveRoom, deleteRoom } from "../utility/api.js";
-
-import {v4 as uuidv4} from 'uuid'; // For creating new Editor Rooms.
-import { set } from "lodash";
+import {useEffect, useState, useRef} from "react";
+import { useNavigate } from "react-router-dom";
+import { createNewEdRoom, getAllRooms, generateInvLink, joinRoomViaInv, leaveRoom, deleteRoom } from "../utility/api.js";
 import { io } from "socket.io-client";
 import ManageUsersSection from "../misc-features/ManageUsersSection.jsx";
 const socket = io("http://localhost:4000");
@@ -14,54 +8,17 @@ const socket = io("http://localhost:4000");
 function Dashboard({ userData, logout, sendRoomID, loadUser, loadRoomUsers, setUser }) {
     const joinEdRoomLink = useRef(null);
     const newEdRoomNameRef = useRef(null);
-    const [invLink, setInvLink] = useState("");
+    const [invLinks, setInvLinks] = useState([]);
     const [rooms, setRooms] = useState([]);
-
-    //const [showManageUsers, setShowManageUsers] = useState(false); <-- problematic!
     const [activeManageUsersRoomId, setActiveManageUsersRoomId] = useState(null);
-
-    const [roomMembers, setRoomMembers] = useState([]); // Related to showManageUsers state var (gets the members of the Room you click "Manage Users" on).
-    
+    const [roomMembers, setRoomMembers] = useState([]);
     const navigate = useNavigate();
-    
+
     const handleLogout = () => {
         logout();
         navigate("/login");
     };
 
-    // 1. useEffect that'll run on mount and double-check that userData is valid:
-    // NOTE: (Adding this well after writing most of the code for the page -- don't need to do the same with roomId since it's obtained directly elsewhere). 
-    useEffect(() => {
-        if(!userData) {
-            const storedUser = localStorage.getItem("userData");
-            if(storedUser) {
-                const parsedUser = JSON.parse(storedUser);
-                setUser(parsedUser);
-            } else {
-                loadUser();
-            }
-        }
-    }, []);
-
-
-    // 2. useEffect that'll run on mount for loading in the Editor Rooms this User has access to:
-    useEffect(() => {
-        const fetchRooms = async() => {
-            const token = localStorage.getItem("token");
-            if(!token) return;
-
-            try {
-                const data = await getAllRooms(token);
-                setRooms(data);
-            } catch (err) {
-                console.error("DEBUG: was not able to fetch rooms oh no!: ", err);
-            }
-        };
-
-        fetchRooms();
-    }, []);
-
-    // To join a new room from the area I'll be loading them:
     const handleJoin = async(roomId) => {
         sendRoomID(roomId);
         // No need to emit any join notifications; I do this in my Editor.jsx file.
@@ -73,7 +30,7 @@ function Dashboard({ userData, logout, sendRoomID, loadUser, loadRoomUsers, setU
         const token = localStorage.getItem("token");
         if(!token) return;
         try {
-            const data = await leaveRoom(roomId, token);
+            await leaveRoom(roomId, token);
 
             // Send an emit to the Socket.IO server indicating resignation of access:
             socket.emit("notification", {
@@ -87,39 +44,30 @@ function Dashboard({ userData, logout, sendRoomID, loadUser, loadRoomUsers, setU
 
             navigate('/dashboard');
         } catch (err) {
-            console.error("DEBUG: Error in attempting to leave the Editor Room ID: ", roomId);
+            console.error(`ERROR: Error in attempting to leave the Editor Room ID:(${roomId}) because: ${err}`);
         }
     }
 
-    // To delete a room:
     const handleDelete = async(roomId) => {
         const token = localStorage.getItem("token");
         if(!token) return;
 
         try {
-            const data = await deleteRoom(roomId, token);
-            if(data.success) {
-                console.log("DEBUG: ROOM SUCCESSFULLY DELETED! => ", data);
-            } else {
-                console.log("DEBUG: ROOM NOT DELETED! => ", data);
-            }
+            await deleteRoom(roomId, token);
             navigate('/dashboard');
         } catch (err) {
-            console.error("DEBUG: Error in attempting to delete the Editor Room ID: ", roomId);
+            console.error(`ERROR: Error in attempting to delete the Editor Room ID:(${roomId}) because: ${err}`);
         }
     }
-    // NOTE:+DEBUG: Don't forget -- when a room is deleted, I get rid of all server records related to it...
 
-    // Function for handling generate link invites:
     const generateInvite = async(roomId) => {
         const token = localStorage.getItem("token");
         if(!token) return;
         try {
             const data = await generateInvLink(roomId, token);
-            setInvLink(data.inviteURL);
-            // FOR NOW...
+            setInvLinks(prev => ({...prev, [roomId]: data.inviteURL}));
         } catch (err) {
-            console.error("DEBUG: Error in attempting to generate invite links...");
+            console.error(`ERROR: Error in attempting to generate an invite link for Room ID:(${roomId}) because: ${err}`);
         }
     };
 
@@ -147,13 +95,38 @@ function Dashboard({ userData, logout, sendRoomID, loadUser, loadRoomUsers, setU
         }
     };
 
-    const debugFunction = () => {
-        console.log("The value of userData => [", userData, "]");
-    };
+    // 1. useEffect that'll run on mount and double-check that userData is valid:
+    useEffect(() => {
+        if(!userData) {
+            const storedUser = localStorage.getItem("userData");
+            if(storedUser) {
+                const parsedUser = JSON.parse(storedUser);
+                setUser(parsedUser);
+            } else {
+                loadUser();
+            }
+        }
+    }, []);
+
+    // 2. useEffect that'll run on mount for loading in the Editor Rooms this User has access to:
+    useEffect(() => {
+        const fetchRooms = async() => {
+            const token = localStorage.getItem("token");
+            if(!token) return;
+
+            try {
+                const data = await getAllRooms(token);
+                setRooms(data);
+            } catch (err) {
+                console.error(`ERROR: Was not able to fetch the Editor Rooms associated with the client user: ${err}`);
+            }
+        };
+        fetchRooms();
+    }, []);
 
     return(
         <div>
-            <h1>DASHBOARD GOES HERE!!!</h1>
+            <h1>DASHBOARD</h1>
 
             <button onClick={handleLogout} >LOG OUT</button>
 
@@ -163,15 +136,11 @@ function Dashboard({ userData, logout, sendRoomID, loadUser, loadRoomUsers, setU
                 <div style={{borderStyle:"solid", borderColor:"red", display:"flex", flexDirection:"column",width:"300px",height:"150px"}}>
                     <h4>DEBUG: CREATE ROOM BUTTON</h4>    
 
-                    {/* This form will connect t othe authController.js or whatever and invoke the Create Table command (more or less): */}
-                    {/* DEBUG: For now, I am doing extremely minimal styling <here styleName={}></here> */}
                     <form onSubmit={ async (e) => {
                         e.preventDefault();
                         const edRoomName = newEdRoomNameRef.current.value;
                         const token = localStorage.getItem("token");
-                        console.log("DEBUG: THE VALUE OF token => [", token, "]");
-                        const data = await createNewEdRoom(edRoomName, token);
-                        console.log("debug: dah value of data => [", data, "]");
+                        await createNewEdRoom(edRoomName, token);
                     }}>
                         <div style={{width:"100%"}}>
                             Give room a name (optional).
@@ -195,13 +164,12 @@ function Dashboard({ userData, logout, sendRoomID, loadUser, loadRoomUsers, setU
                             const data = await joinRoomViaInv(token, edRoomLink);
                             console.log("DEBUG: The value of data => [", data, "]");
                             if(data.success) {
-                                // refresh page? (May not be needed).
                                 navigate("/dashboard");
                             } else {
                                 alert("Seems to be an expired link or you might already be in the room.");
                             }
                         } catch (err) {
-                            console.error("Failed to join Editor Room via invite -- probably invalid link or something.");
+                            console.error(`ERROR: Failed to join Editor Room via invite. The reason: ${err}`);
                         }
                     }}>
                         <button>JOIN NEW ROOM</button>
@@ -210,12 +178,10 @@ function Dashboard({ userData, logout, sendRoomID, loadUser, loadRoomUsers, setU
                 </div>
             </div>
 
-
             {/* EVERYTHING BELOW IS WHERE THE "ROOM LOADING" STATIC CODE IS -- REMEMBER THAT FOR LATER WHEN REORGANIZING!!! */}
-            <h4>LOAD ROOMS HERE:</h4>
+            <h4>YOUR EDITOR ROOMS:</h4>
             <div style={{borderStyle:"solid", borderColor:"purple"}}>
                 {rooms.map((room) => {
-                    console.log("AHHHHHHHHHHHHHHHHHHH");
                     return(
                         <div key={room.user_room_id} style={{borderStyle:"solid"}}>
                             <div>
@@ -224,41 +190,34 @@ function Dashboard({ userData, logout, sendRoomID, loadUser, loadRoomUsers, setU
                                 <p>Role: {room.role}</p>
                             </div>
                             
-                            {/* JOIN ROOM BUTTON: */}
+                            {/* Join Room Button: */}
                             <button onClick={()=> handleJoin(room.room_id)} >
                                 JOIN ROOM
                             </button>
 
-                            {/* GENERATE INVITE LINK BUTTON: */}
+                            {/* Generate Invite Link Button: */}
                             <div style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
                                 <button onClick={()=> generateInvite(room.room_id)}>GET INVITE</button>
-                                {/* DEBUG: I want some text that goes "Your Inv Link:"[ Rect where text is dynamically allocated ] */}
-                                Your Invite Link: <p style={{ borderStyle:"solid", height:"20px", width:"500px" }}>{invLink}</p>
-                                {/* NOTE:+DEBUG: ^ this is what I have **FOR NOW** Think it'd be better to have a pop-up afterwards... */}
-                                <button onClick={()=> navigator.clipboard.writeText(invLink) }>COPY LINK</button>
-                                {/* NOTE:+DEBUG: For now, let's just make this <button> copy the state variable value to the clipboard.
-                                When I polish the site a little more... I can have like the same thing, but when you close the pop-up,
-                                then the state variable will reset back to "" empty.*/}
+                                Your Invite Link: <p style={{ borderStyle:"solid", height:"20px", width:"500px" }}>{invLinks[room.room_id]}</p>
+                                <button onClick={()=> navigator.clipboard.writeText(invLinks[room.room_id] || "") }>COPY LINK</button>
                             </div>
 
-                            {/* Want a button here that lets you LEAVE the room. */}
-                            <button onClick={()=>handleLeave(room.room_id)}>
-                                LEAVE ROOM
-                            </button>
+                            {/* Want a button here that lets you LEAVE the room (which you can NOT do if you're the owner/"king"). */}
+                            {room.role === 'member' && (
+                                <>
+                                    <button onClick={()=>handleLeave(room.room_id)}>
+                                        LEAVE ROOM
+                                    </button>
+                                </>
+                            )}
 
-                            {/* Want a button here that lets you DELETE the room. */}
-                            <button onClick={()=>handleDelete(room.room_id)}>
-                                DELETE ROOM
-                            </button>
-
-                            {/* Want a button here that lets you manage users: */}
-                            <button onClick={()=>handleManageUsers(room.room_id)}>
-                                MANAGE USERS
-                            </button>
-                            {/* ^^^ NOTE:+DEBUG:+TO-DO:
-                            - I want to make it so that the "MANAGE USERS" button is inaccessible if the User is NOT the Owner/King of this server.
-                            - Way I think I'll do that is, when I revamp the Room loading feature, I'll maybe make that change there so that
-                            this button just wouldn't be accessible + the other ones like DELETE ROOM which require Owner status. */}
+                            {/* Buttons for deleting the Room and managing its users (kicking them/transferring ownership) -- for the "Owner" only: */}
+                            {room.role === 'king' && (
+                                <>
+                                    <button onClick={()=>handleDelete(room.room_id)}>DELETE ROOM</button>
+                                    <button onClick={()=>handleManageUsers(room.room_id)}>MANAGE USERS</button>
+                                </>
+                            )}
 
                             {/* Code below is for adding a "shadowed" background when the Manage Users component appears (should come before): */}
                             {activeManageUsersRoomId !== null && (
@@ -289,21 +248,10 @@ function Dashboard({ userData, logout, sendRoomID, loadUser, loadRoomUsers, setU
                                 roomMembers={roomMembers} 
                                 onClose={()=> {setActiveManageUsersRoomId(null); setRoomMembers([]); }}/>
                             )}
-
-                            {/* DEBUG: */}
-                            <button onClick={()=> debugFunction()}>
-                                DEBUG!
-                            </button>
-
-                            {/* DEBUG:+NOTE: Maybe you only get the "leave" button if you're non-owner. Only get "delete" if you're the owner. */}
-                            {/* ^ I can have them still be there but grayed out when I attempt to click them! with hover text that says "You the owner!" */}
-
                         </div>
                     )
                 })}
             </div>
-            {/* EVERYTHING ABOVE IS WHERE THE "ROOM LOADING" STATIC CODE IS -- REMEMBER THAT FOR LATER WHEN REORGANIZING!!! */}
-
         </div>
     );
 }
