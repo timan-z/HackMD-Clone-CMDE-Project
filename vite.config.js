49c75e667138a30107021aa41097c9a987354377
timan-z <58091424+timan-z@users.noreply.github.com>
2025-08-16 10:10:43 -0400

    Probably a better fix that fixes things smoothly for Netlify and developing locally.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), wasm()],
+  resolve: {
+    alias: {
+      "rust-markdown": path.resolve(__dirname, "./rust-markdown/pkg/rust_markdown.js")
+    }
+  },
 });
